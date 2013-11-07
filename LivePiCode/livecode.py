7babb41028a23ba799c126e80ac37571e518faad
Justin <justin.poh@students.olin.edu>
2013-11-07 15:17:32 -0500

    Updating all of my changes to the raspberry pi code

---
+++ LivePiCode/livecode.py
@@ -0,0 +1,7 @@
+"""
+This module contains finalized code that is meant to run on the raspberry pi for the dominobot.
+
+This module is meant to be considered as "live" - Test code or code that has not been verified should not be included in this module.
+
+Created by team dominobot for Principles of Engineering, Fall 2013
+""" 
